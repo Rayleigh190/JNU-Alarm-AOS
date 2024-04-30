369badd69a3d85a2e07037f04f9a3572e826e190
rayleigh <rayleigh190@gmail.com>
2024-04-30 15:06:24 +0900

    feat: FAQ 및 푸시 알림 요청 메뉴 추가

--- app/src/main/java/com/jnu_alarm/android/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/jnu_alarm/android/ui/settings/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.jnu_alarm.android.ui.settings;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.navigation.Navigation;
@@ -9,6 +10,7 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import com.jnu_alarm.android.BuildConfig;
 import com.jnu_alarm.android.R;
+import com.jnu_alarm.android.WebActivity;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -34,6 +36,22 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         findPreference("contact").setOnPreferenceClickListener(preference -> {
             Navigation.findNavController(requireView()).navigate(R.id.action_navigation_settings_to_navigation_contact);
+            return true;
+        });
+
+        findPreference("PUSH").setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse("https://wackitlab.notion.site/2800db7021a24e59adcd4f27f1673be4");
+            intent.setData(uri);
+            startActivity(intent);
+            return true;
+        });
+
+        findPreference("FAQ").setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getContext(), WebActivity.class);
+            intent.putExtra("link", "https://wackitlab.notion.site/FAQ-b0f2438e25574315baa0962d1dd250e5");
+            intent.putExtra("title", "FAQ");
+            startActivity(intent);
             return true;
         });
 
