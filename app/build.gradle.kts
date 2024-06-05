067d6e9de63d41e7229a7b28e67768f09c4ad14e
rayleigh <rayleigh190@gmail.com>
2024-06-05 12:19:27 +0900

    update: version updated to v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.jnu_alarm.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.2"
+        versionCode = 8
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
